51fe5b9b24170f5a960641c0995071eb8f992c18
João Falcão <joaofalcaobrito@gmail.com>
2025-11-21 19:48:50 -0300

    paginainicial

--- Groov/src/App.jsx
+++ Groov/src/App.jsx
@@ -1,24 +1,25 @@
-
 import { Link } from "react-router"
 
 function App() {
 
   return (
-    <>
-      <main className='min-h-screen flex flex-col items-center m-auto justify-center'>
-        <button className='border-4 border-roxop w-full py-[0.8rem] rounded-[2.5rem]'>
-          <Link className='text-[1.25rem] text-roxop font-bold' to="login">Já possuo uma conta</Link>
-        </button>
-        <div className='flex justify-between items-center gap-2 w-full'>
-          <hr className='border-roxos rounded-2xl border w-36'/>
-          <p className='text-[1rem] font-bold text-roxos'>ou</p>
-          <hr className='border-roxos border w-36'/>
-        </div>
-        <button className='w-full bg-roxop py-[0.8rem] rounded-[2.5rem]'>
-          <Link to="/criarConta" className='text-[1.25rem] text-white font-bold'>Criar conta</Link>
-        </button>
-      </main>
-    </>
+    <main className="fixed inset-0 flex justify-center items-center flex-col w-full max-w-sm mx-auto gap-4">
+
+      <button className='border-4 border-roxop w-full py-[0.8rem] rounded-[2.5rem]'>
+        <Link className='text-[1.25rem] text-roxop font-bold' to="login">Já possuo uma conta</Link>
+      </button>
+
+      <div className='flex justify-between items-center gap-2 w-full'>
+        <hr className='border-roxos rounded-2xl border w-36'/>
+        <p className='text-[1rem] font-bold text-roxos'>ou</p>
+        <hr className='border-roxos border w-36'/>
+      </div>
+
+      <button className='w-full bg-roxop py-[0.8rem] rounded-[2.5rem]'>
+        <Link to="/criarConta" className='text-[1.25rem] text-white font-bold'>Criar conta</Link>
+      </button>
+
+    </main>
   )
 }
 
